d01ddef6c0082baef86471e43ec906eee3abde73
John <johnsjobs7@gmail.com>
2022-05-22 16:24:01 +0100

    continued getting upto speed with vue3.

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('vue-non-component-basic');
+    return view('index');
+    //return view('vue-non-component-basic');
 });
